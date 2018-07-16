4a6ed9e35448c5cd178591c205bd6c6a901eeaa6
jusgolds <jusgolds@gmail.com>
2018-07-15 19:58:50 -0500

    Finished variables exercises

--- course_100/intro_to_programming/variables/name.rb
+++ course_100/intro_to_programming/variables/name.rb
@@ -9,3 +9,11 @@ puts "Hello, #{name}!"
 10.times do
   puts name
 end
+
+#3
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+
+puts first_name + " " + last_name
