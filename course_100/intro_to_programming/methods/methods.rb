7a751c9e5fc51a7da73c9b3e3778c6e3b3a3cc73
jusgolds <jusgolds@gmail.com>
2018-07-16 15:21:20 -0500

    Methods chapter

---
+++ course_100/intro_to_programming/methods/methods.rb
@@ -0,0 +1,13 @@
+#1
+def greeting(name)
+  "hello, " + name + "! How are you?"
+end
+
+puts greeting("Justin")
+
+#3
+def multiply(number1, number2)
+    number1 * number2
+end
+
+puts multiply(3, 4)
