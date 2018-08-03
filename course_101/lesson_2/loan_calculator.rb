3d6f7fb1eded819d12f07d829d99ccf2115d186c
jusgolds <jusgolds@gmail.com>
2018-08-02 20:47:36 -0500

    Add loan_calculator

---
+++ course_101/lesson_2/loan_calculator.rb
@@ -0,0 +1,15 @@
+# Need the following information:
+# => the loan amount
+# => the APR
+# => the loan duration
+
+# Need to calculate the following things:
+# => monthly interest rate
+# => loan duration in months
+
+# algorithm
+# => m = p * (j / (1 - (1 + j)**(-n)))
+# m = monthly payment
+# p = loan amount
+# j = monthly interest rate
+# n = loan duration in months
