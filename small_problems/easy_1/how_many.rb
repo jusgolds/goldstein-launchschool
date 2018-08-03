ffaa2049d02e2d01455415c3990ddf153ae55fb6
jusgolds <jusgolds@gmail.com>
2018-08-02 21:04:38 -0500

    Add how_many.rb

---
+++ small_problems/easy_1/how_many.rb
@@ -0,0 +1,12 @@
+# Write a method that counts the number of occurances of each element
+# in a given array
+
+# Expected Output
+# car => 4
+# truck => 3
+# SUV => 1
+# motorcycle => 2
+
+vehicles = ['car', 'car', 'truck', 'car', 'SUV', 'truck', 'motorcycle', 'motorcycle', 'car', 'truck']
+
+count_occurrences(vehicles)
