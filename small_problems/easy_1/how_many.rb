9b065d51ba3b95d067a629b4881c5c6d708f579e
jusgolds <jusgolds@gmail.com>
2018-08-02 21:20:44 -0500

    Update how_many

--- small_problems/easy_1/how_many.rb
+++ small_problems/easy_1/how_many.rb
@@ -7,6 +7,13 @@
 # SUV => 1
 # motorcycle => 2
 
+
 vehicles = ['car', 'car', 'truck', 'car', 'SUV', 'truck', 'motorcycle', 'motorcycle', 'car', 'truck']
+
+def count_occurrences(array)
+  counts = Hash.new(0)
+  array.each { |name| counts[name] += 1 }
+  counts.each { |name, count| puts "#{name} => #{count}" }
+end
 
 count_occurrences(vehicles)
