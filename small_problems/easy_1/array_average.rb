6c7beec545c991291103e27110d56193dd2d90b9
jusgolds <jusgolds@gmail.com>
2018-08-07 14:45:35 -0500

    Complete array_average

--- small_problems/easy_1/array_average.rb
+++ small_problems/easy_1/array_average.rb
@@ -9,8 +9,12 @@
 
 
 def average(arr)
+  total = 0
+  arr.select do |num|
+    total += num
+  end
+  average = total / arr.length
 end
-
 
 puts average([1, 5, 87, 45, 8, 8]) == 25
 puts average([9, 47, 23, 95, 16, 52]) == 40
