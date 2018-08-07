53ee56a04e26be635b656408799a4740ae216f17
jusgolds <jusgolds@gmail.com>
2018-08-07 11:01:16 -0500

    Add sudo-code

--- small_problems/easy_1/reverse_it_2.rb
+++ small_problems/easy_1/reverse_it_2.rb
@@ -2,8 +2,16 @@
 # if word has 5 or more letters, reverses order of letters
 # prints the words
 
+# split the words in the string into indivdual words
+# stick these words in an array
+# loop through array and..
+# if word has less than 5 letters, skip
+# if word has 5 or more letters, reverse the order of the letters
+# print the array
+
 
 def reverse_words(string)
+  string.reverse
 end
 
 puts reverse_words('Professional')          # => lanoisseforP
