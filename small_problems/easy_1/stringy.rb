d876b407fe4d9842299892cd0c0dbd00cce12bd3
jusgolds <jusgolds@gmail.com>
2018-08-07 14:10:58 -0500

    Add sudo code

--- small_problems/easy_1/stringy.rb
+++ small_problems/easy_1/stringy.rb
@@ -2,6 +2,15 @@
 # the method returns alternating 1s and 0s starting with 1
 # this alternating number has a length equal to the argument
 
+# convert num to an integer
+# assign that integer to a counter variable
+# create string variable for return value
+# start a loop that counts down
+# if .odd? add a 1/join at the end
+# if .even? add a 0
+# when the counter runs out, break
+# print the string
+
 def stringy(num)
 end
 
