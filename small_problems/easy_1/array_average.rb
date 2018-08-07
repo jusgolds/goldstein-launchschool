14fd53da96d7de70e3cf2b6cd0fd3e4325265801
jusgolds <jusgolds@gmail.com>
2018-08-07 14:30:24 -0500

    Add array_average

---
+++ small_problems/easy_1/array_average.rb
@@ -0,0 +1,9 @@
+# write an array that takes one argument, an array with integers
+# returns the average of the numbers in the array
+
+def average(arr)
+end
+
+
+puts average([1, 5, 87, 45, 8, 8]) == 25
+puts average([9, 47, 23, 95, 16, 52]) == 40
