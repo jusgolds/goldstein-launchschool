815b014b44d727f1b7ac080e8016c289e017d863
jusgolds <jusgolds@gmail.com>
2018-08-07 10:54:26 -0500

    Start reverse_it_2

---
+++ small_problems/easy_1/reverse_it_2.rb
@@ -0,0 +1,11 @@
+# method takes one string argument
+# if word has 5 or more letters, reverses order of letters
+# prints the words
+
+
+def reverse_words(string)
+end
+
+puts reverse_words('Professional')          # => lanoisseforP
+puts reverse_words('Walk around the block') # => Walk dnuora the kcolb
+puts reverse_words('Launch School')         # => hcnuaL loohcS
