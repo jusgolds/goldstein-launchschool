62db70c18adc79b2df8611992217707967fc5f9a
jusgolds <jusgolds@gmail.com>
2018-08-07 14:33:47 -0500

    Sudo code

--- small_problems/easy_1/array_average.rb
+++ small_problems/easy_1/array_average.rb
@@ -1,6 +1,13 @@
 # write an array that takes one argument, an array with integers
 # returns the average of the numbers in the array
 
+# create total variable = 0
+# start a loop that goes through each index of the array
+# for each index, add that number to the total variable
+# figure out the size of the array
+# divide the total / size
+
+
 def average(arr)
 end
 
