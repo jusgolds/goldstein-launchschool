1cd1503d160d027cdbec6b9d72116eb2299f607d
jusgolds <jusgolds@gmail.com>
2018-08-07 14:03:32 -0500

    Add stringy.rb

---
+++ small_problems/easy_1/stringy.rb
@@ -0,0 +1,12 @@
+# write a method that takes one argument, an integer
+# the method returns alternating 1s and 0s starting with 1
+# this alternating number has a length equal to the argument
+
+def stringy(num)
+end
+
+
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
