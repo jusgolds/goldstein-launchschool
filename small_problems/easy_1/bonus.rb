9e2a81ebf95de69ac5fd26d755d0f7f5db86afa7
jusgolds <jusgolds@gmail.com>
2018-08-08 15:32:24 -0500

    Add bonus

---
+++ small_problems/easy_1/bonus.rb
@@ -0,0 +1,17 @@
+# method takes 2 arguments, int and boolean
+# if boolean is true, half the salary (int)
+# if false, bonus if 0
+
+# write an if statement:
+# if true, divide the salary by 2, print that bonus
+# if false, print a bonus of 0
+
+
+def calculate_bonus(salary, bool)
+
+end
+
+
+#puts calculate_bonus(2800, true) == 1400
+#puts calculate_bonus(1000, false) == 0
+#puts calculate_bonus(50000, true) == 25000
