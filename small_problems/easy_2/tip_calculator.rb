5bf539d3c5571c5cebe3439f12ad34ce57b2a20a
jusgolds <jusgolds@gmail.com>
2018-08-08 16:06:26 -0500

    Sudo code

--- small_problems/easy_2/tip_calculator.rb
+++ small_problems/easy_2/tip_calculator.rb
@@ -7,3 +7,8 @@
 
 # => The tip is $30.0
 # => The total is $230.0
+
+# ask for the bill and save it in a bill(pre tip) variable
+# ask for a tip percentage and save it in a tip percent variable
+# calculate the tip and display it
+# add the tip to the pre-tip total and display that total
