28331fb1689de3ad931037ff845588e69543d89f
jusgolds <jusgolds@gmail.com>
2018-08-08 15:43:11 -0500

    Add sudo code

---
+++ small_problems/easy_2/teddy_age.rb
@@ -0,0 +1,6 @@
+# build a program that randomly generates a number (Teddy's age)
+# beteween 20 - 200
+# then it prints out the age within a sentence
+
+# create variable and have it equal to a random number 20-200
+# print the sentence Teddy is (insert age) years old!
