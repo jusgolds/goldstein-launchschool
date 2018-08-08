d00b2bee43f27b593912fb03d76b17315e3cf993
jusgolds <jusgolds@gmail.com>
2018-08-08 16:04:30 -0500

    Add tip_calculator

---
+++ small_problems/easy_2/tip_calculator.rb
@@ -0,0 +1,9 @@
+# create a simple tip calculator
+# prompt for the bill amount and the tip rate
+# display the tip and the total (tip included)
+
+# => What is the bill? 200
+# => What is the tip percentage? 15
+
+# => The tip is $30.0
+# => The total is $230.0
