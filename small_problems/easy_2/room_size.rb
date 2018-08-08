7f2aad753c51f6a3a98eef576fb07d70a04bebf4
jusgolds <jusgolds@gmail.com>
2018-08-08 15:52:18 -0500

    Sudo code

---
+++ small_problems/easy_2/room_size.rb
@@ -0,0 +1,17 @@
+# ask user for room length and width in meters
+# display the area in meters and feet
+
+# => Enter the length of the room in meters:
+# => 10
+# => Enter the width of the room in meters:
+# => 7
+# => The area of the room is 70.0 square meters (753.47 square feet).
+
+# create variables for name, length(meters), width(meters), area(meters), area(feet)
+# print a statement asking for the length in meters
+# make this an int and add to the variable
+# print a statement asking for the width in meters
+# make this an int and add to the variable
+# multiply length and width and add to area(meter)
+# convert area(meter) to feet and add to area(feet)
+# print both the area in meters and feet
