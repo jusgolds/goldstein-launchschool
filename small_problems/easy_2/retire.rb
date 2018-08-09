ab8eaf31cb9a104fb9ae088565979d1e2aacc3c5
jusgolds <jusgolds@gmail.com>
2018-08-09 13:12:28 -0500

    Sudo code

--- small_problems/easy_2/retire.rb
+++ small_problems/easy_2/retire.rb
@@ -5,3 +5,8 @@
 
 # => It's 2016. You will retire in 2056.
 # => You have only 40 years of work to go!
+
+# ask the user for their age and save that into a variable
+# ask the user what age they want to retire and save that into variable
+# calculate years to retirement by subtracting retirement age from current age
+# add those years to current year (2018)
