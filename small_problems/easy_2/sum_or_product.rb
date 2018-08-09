f51b4f10af78b685e68264fadde2cace25594547
jusgolds <jusgolds@gmail.com>
2018-08-09 13:37:32 -0500

    Add sum_or_product

---
+++ small_problems/easy_2/sum_or_product.rb
@@ -0,0 +1,3 @@
+# Write a program that asks the user to enter an integer greater than 0
+# then asks if the user wants to determine the sum or product
+# then calculate sum or product of all numbers between 1 and the entered integer.
