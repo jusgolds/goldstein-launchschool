f289b1f648266ecec313e1e5a66228916f8206ef
jusgolds <jusgolds@gmail.com>
2018-08-09 13:41:15 -0500

    Add sum_or_product

--- small_problems/easy_2/sum_or_product.rb
+++ small_problems/easy_2/sum_or_product.rb
@@ -1,3 +1,17 @@
 # Write a program that asks the user to enter an integer greater than 0
 # then asks if the user wants to determine the sum or product
 # then calculate sum or product of all numbers between 1 and the entered integer.
+
+# => >> Please enter an integer greater than 0:
+# => 5
+# => >> Enter 's' to compute the sum, 'p' to compute the product.
+# => s
+# => The sum of the integers between 1 and 5 is 15.
+
+# ask the user to enter an integer greater than 0, store in variable
+# ask user to either compute sum, or product
+# if s
+# while loop where sum += 1 until counter = variable
+# counter += 1
+# if p
+# same while with multiplication
