85f0ebf8cce23010d84483d1cb249a0da6836d3c
jusgolds <jusgolds@gmail.com>
2018-08-09 13:17:31 -0500

    Remove current year constant and add Time.now.year

--- small_problems/easy_2/retire.rb
+++ small_problems/easy_2/retire.rb
@@ -11,15 +11,15 @@
 # calculate years to retirement by subtracting retirement age from current age
 # add those years to current year (2018)
 
-CURRENT_YEAR = 2018
+current_year = Time.now.year
 print "What is your age? "
 age = gets.chomp.to_i
 
 print "At what age would you like to retire? "
 retire_age = gets.chomp.to_i
 
-retire_year = CURRENT_YEAR + (retire_age - age)
-years_to_retire = retire_year - CURRENT_YEAR
+retire_year = current_year + (retire_age - age)
+years_to_retire = retire_year - current_year
 
-puts "It's #{CURRENT_YEAR}. You will retire in #{retire_year}."
+puts "It's #{current_year}. You will retire in #{retire_year}."
 puts "You have only #{years_to_retire} years of work to go!"
