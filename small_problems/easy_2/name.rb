a65aa66754c3dc5d749030b7adab69d4ef709d99
jusgolds <jusgolds@gmail.com>
2018-08-09 13:20:12 -0500

    Add name.rb

---
+++ small_problems/easy_2/name.rb
@@ -0,0 +1,8 @@
+# Write a program that will ask for user's name.
+# The program will then greet the user.
+# If the user writes "name!" then the computer yells back to the user.
+
+# => What is your name? Bob
+# => Hello Bob.
+# => What is your name? Bob!
+# => HELLO BOB. WHY ARE WE SCREAMING?
