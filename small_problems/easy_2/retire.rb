e78e04d8234b70b952aa3a4b18a95a2933d09bac
jusgolds <jusgolds@gmail.com>
2018-08-09 13:16:15 -0500

    Complete retire

--- small_problems/easy_2/retire.rb
+++ small_problems/easy_2/retire.rb
@@ -10,3 +10,16 @@
 # ask the user what age they want to retire and save that into variable
 # calculate years to retirement by subtracting retirement age from current age
 # add those years to current year (2018)
+
+CURRENT_YEAR = 2018
+print "What is your age? "
+age = gets.chomp.to_i
+
+print "At what age would you like to retire? "
+retire_age = gets.chomp.to_i
+
+retire_year = CURRENT_YEAR + (retire_age - age)
+years_to_retire = retire_year - CURRENT_YEAR
+
+puts "It's #{CURRENT_YEAR}. You will retire in #{retire_year}."
+puts "You have only #{years_to_retire} years of work to go!"
