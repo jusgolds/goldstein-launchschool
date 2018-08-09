069c6124193e658d29771c62a12f93e2c23ceff6
jusgolds <jusgolds@gmail.com>
2018-08-09 13:30:43 -0500

    Sudo code

--- small_problems/easy_2/odd_numbers.rb
+++ small_problems/easy_2/odd_numbers.rb
@@ -1,2 +1,9 @@
 # Print all odd numbers from 1 to 99, inclusive.
 # All numbers should be printed on separate lines.
+
+# create counter = 1
+# while counter < 100
+# if counter is odd
+# puts counter
+# if even, skip
+# counter += 1
