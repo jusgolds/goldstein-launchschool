e3458432bf2676d9a403fe2c7b4da9bf507618fb
jusgolds <jusgolds@gmail.com>
2018-08-09 13:33:59 -0500

    Sudo code

--- small_problems/easy_2/even_numbers.rb
+++ small_problems/easy_2/even_numbers.rb
@@ -1,2 +1,9 @@
 #Print the even numbers from 1 to 99, inclusive.
 #All numbers should be printed on separate lines.
+
+# create counter = 1
+# while counter < 100
+# if counter is even
+# puts counter
+# if odd, skip
+# counter += 1
