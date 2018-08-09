ee3462592f46b79bb4ac0eb49e3fc971df26e5a2
jusgolds <jusgolds@gmail.com>
2018-08-09 13:10:46 -0500

    Add retire

---
+++ small_problems/easy_2/retire.rb
@@ -0,0 +1,7 @@
+# build a program that displays when the user will retire and how many years to go
+
+# => What is your age? 30
+# => At what age would you like to retire? 70
+
+# => It's 2016. You will retire in 2056.
+# => You have only 40 years of work to go!
