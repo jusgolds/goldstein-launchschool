5cf5954c1d95510cb6257edf71ed28c18d3a1ef3
jusgolds <jusgolds@gmail.com>
2018-08-10 13:46:32 -0500

    Complete rock_paper_scissors

--- course_101/lesson_2/rock_paper_scissors.rb
+++ course_101/lesson_2/rock_paper_scissors.rb
@@ -1,19 +1,45 @@
 VALID_CHOICES = ['rock', 'paper', 'scissors']
 
 def prompt(message)
-  Kernel.puts("=> message")
+  Kernel.puts("=> #{message}")
 end
 
-loop do
-  prompt("Pick one: #{VALID_CHOCIES.join(", ")}")
-  choice = Kernel.gets().chomp()
-
-  if VALID_CHOICES.include?(choice)
-    break
+def display_result(player, computer)
+  if (player == 'rock' && computer == 'scissors') ||
+      (player == 'paper' && computer == 'rock') ||
+      (player == 'scissors' && computer == 'paper')
+    prompt("You won!")
+  elsif (player == 'rock' && computer == 'paper') ||
+        (player == 'paper' && computer == 'scissors') ||
+        (player == 'scissors' && computer == 'rock')
+    prompt("The computer won!")
   else
-    prompt("That is not a valid choice.")
+    prompt("It's a tie!")
   end
 end
 
-# => 8:33
-computer_choice = VALID_CHOICES.sample
+loop do
+  choice = ''
+  loop do
+    prompt("Pick one: #{VALID_CHOICES.join(", ")}")
+    choice = Kernel.gets().chomp()
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That is not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt("You chose #{choice}; Computer chose #{computer_choice}")
+
+  display_result(choice, computer_choice)
+
+  prompt("Do you want to play again?")
+  answer = gets().chomp()
+  break unless answer.downcase().start_with?("y")
+end
+
+prompt("Thank you for playing. Good bye!")
