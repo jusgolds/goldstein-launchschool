19638638074c24d081087847d09dcfbc40d4d1a6
jusgolds <jusgolds@gmail.com>
2018-08-15 12:31:45 -0500

    Sudo code

--- small_problems/easy_3/counting_characters.rb
+++ small_problems/easy_3/counting_characters.rb
@@ -1,3 +1,9 @@
 # => Write a program that will ask a user for an input of a word
 # => or multiple words and give back the number of characters
 # => Spaces should not be counted as a character
+
+# prompt the user to add a word or words
+# store that string in var "words"
+# iterate through string
+# for each character, +=1 to var "count"
+# print number of characters in string
