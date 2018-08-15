98335995b8fbcd805d543ffedcb2c42206a8ca93
jusgolds <jusgolds@gmail.com>
2018-08-15 11:04:44 -0500

    Sudo code, first iteration

--- small_problems/easy_3/searching_101.rb
+++ small_problems/easy_3/searching_101.rb
@@ -1,3 +1,12 @@
 # => Write a program that solicits 6 numbers from the user
 # => then prints a message that describes whether or not
 # => the 6th number appears amongst the first 5 numbers.
+
+# first iteration
+# create a count variable
+# create method to prompt user to add a number
+# start loop
+# when user types number, change the string to in
+# add this int to an arr
+# add 1 to count
+# after 6, put the array
