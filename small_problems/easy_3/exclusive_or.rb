db36bfd930e89ec57d0d16c513c4ee67241f1721
jusgolds <jusgolds@gmail.com>
2018-08-15 14:59:27 -0500

    Complete

--- small_problems/easy_3/exclusive_or.rb
+++ small_problems/easy_3/exclusive_or.rb
@@ -1,3 +1,23 @@
 # => write a method named xor that takes two arguments
 # => and returns true if exactly one of its arguments is truthy
 # => false otherwise.
+
+# create method xor? with 2 arguments
+# arg1 = true or false depending on argument
+# arg2 = true or false depending on argument
+# if arg1 is true and arg 2 is false
+# or if arg2 is true and arg1 is false
+# return true
+# otherwise return false
+
+def xor?(arg1, arg2)
+  return true if arg1 && !arg2
+  return true if arg2 && !arg1
+  false
+end
+
+
+p xor?(5.even?, 4.even?)
+p xor?(5.odd?, 4.odd?)
+p xor?(5.odd?, 4.even?)
+p xor?(5.even?, 4.odd?)
