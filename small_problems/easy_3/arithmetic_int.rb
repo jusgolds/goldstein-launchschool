2fdd3c8df3c3e46b6706c1f13f8fa02744ca9f6b
jusgolds <jusgolds@gmail.com>
2018-08-15 11:56:38 -0500

    Collect 2 ints and solve all operations

--- small_problems/easy_3/arithmetic_int.rb
+++ small_problems/easy_3/arithmetic_int.rb
@@ -1,3 +1,30 @@
 # => Write a program that prompts the user for two positive integers
 # => and then prints the results of the following operations on ints
 # => addition, subtraction, product, quotient, remainder, and power
+
+# create prompt method for formatting
+# create int1 var and ask user for number to store in it, to_i
+# create int2 var and ask user for number to store in it, to_i
+# print each line while solving operations
+
+def prompt(message)
+  puts "==> #{message}"
+end
+
+prompt("Enter the first number:")
+int1 = gets.chomp.to_i
+
+prompt("Enter the second number:")
+int2 = gets.chomp.to_i
+
+prompt("#{int1} + #{int2} = #{int1 + int2}")
+
+prompt("#{int1} - #{int2} = #{int1 - int2}")
+
+prompt("#{int1} * #{int2} = #{int1 * int2}")
+
+prompt("#{int1} / #{int2} = #{int1 / int2}")
+
+prompt("#{int1} % #{int2} = #{int1 % int2}")
+
+prompt("#{int1} ** #{int2} = #{int1**int2}")
