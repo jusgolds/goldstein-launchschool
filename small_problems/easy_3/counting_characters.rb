576668fb348f04c2bcfea130685878b2a9a3f751
jusgolds <jusgolds@gmail.com>
2018-08-15 12:47:45 -0500

    Receive input, remove spaces, count char, print result

--- small_problems/easy_3/counting_characters.rb
+++ small_problems/easy_3/counting_characters.rb
@@ -4,6 +4,18 @@
 
 # prompt the user to add a word or words
 # store that string in var "words"
+# get rid of any spaces
 # iterate through string
 # for each character, +=1 to var "count"
 # print number of characters in string
+
+count = 0
+
+print "Please write word or multiple words: "
+words = gets.chomp
+
+word = words.delete(' ')
+
+word.each_char { count += 1 }
+
+puts "There are #{count} characters in \"#{words}\""
