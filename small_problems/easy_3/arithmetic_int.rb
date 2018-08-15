7938c565b28e8b74283dacdcdfce7e91593c4037
jusgolds <jusgolds@gmail.com>
2018-08-15 11:49:06 -0500

    Add artihmetic_int

---
+++ small_problems/easy_3/arithmetic_int.rb
@@ -0,0 +1,3 @@
+# => Write a program that prompts the user for two positive integers
+# => and then prints the results of the following operations on ints
+# => addition, subtraction, product, quotient, remainder, and power
