c765b86e4e33e98254c0f6c7eedd5bdc5726c4d0
jusgolds <jusgolds@gmail.com>
2018-08-18 16:55:20 -0500

    Add palindromic_2

---
+++ small_problems/easy_3/palindromic_2.rb
@@ -0,0 +1,4 @@
+# => Write another method that returns true
+# => if the string passed as an argument is a palindrome, false otherwise
+# => This time, however, your method should be case-insensitive
+# => and it should ignore all non-alphanumeric characters.
