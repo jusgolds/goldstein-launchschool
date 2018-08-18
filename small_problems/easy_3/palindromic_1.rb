da262508c63fd75521abe80016a2e2004c115a36
jusgolds <jusgolds@gmail.com>
2018-08-18 16:50:28 -0500

    Add palindromic_1

---
+++ small_problems/easy_3/palindromic_1.rb
@@ -0,0 +1,4 @@
+# => Write a method that returns true
+# => if the string passed as an argument is a palindrome, false otherwise
+# => a palindrome reads the same forward and backward
+# => For this exercise, case matters as does punctuation and spaces.
