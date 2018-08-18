6442167d6ac953538472ab7fefd851cec6abe323
jusgolds <jusgolds@gmail.com>
2018-08-18 17:07:22 -0500

    Complete palindromic_2

--- small_problems/easy_3/palindromic_2.rb
+++ small_problems/easy_3/palindromic_2.rb
@@ -7,3 +7,19 @@
 # new_str = remove str spaces and punctuation
 # new_str downcase
 # str == new_str
+
+def palindrome?(str)
+  str == str.reverse
+end
+
+def real_palindrome?(str)
+  str = str.downcase.delete('^a-z0-9')
+  palindrome?(str)
+end
+
+p real_palindrome?('madam') == true
+p real_palindrome?('Madam') == true           # (case does not matter)
+p real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
+p real_palindrome?('356653') == true
+p real_palindrome?('356a653') == true
+p real_palindrome?('123ab321') == false
