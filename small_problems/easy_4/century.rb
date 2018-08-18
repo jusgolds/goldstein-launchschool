c9079b2d226a6835f377713e2378fcf61a03df3c
jusgolds <jusgolds@gmail.com>
2018-08-18 17:38:34 -0500

    Add century.rb

---
+++ small_problems/easy_4/century.rb
@@ -0,0 +1,4 @@
+# => Write a method that takes a year as input and returns the century
+# => The return value should be a string that begins with
+# => the century number
+# => and ends with st, nd, rd, or th as appropriate for that number.
