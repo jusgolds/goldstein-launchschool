fd1aad57e8a1f57183c1fa8d568ee79030e3cc1c
jusgolds <jusgolds@gmail.com>
2018-08-18 16:51:47 -0500

    Sudo code

--- small_problems/easy_3/palindromic_1.rb
+++ small_problems/easy_3/palindromic_1.rb
@@ -2,3 +2,7 @@
 # => if the string passed as an argument is a palindrome, false otherwise
 # => a palindrome reads the same forward and backward
 # => For this exercise, case matters as does punctuation and spaces.
+
+# def method palindrome?(str)
+# str == str.reverse
+# end
