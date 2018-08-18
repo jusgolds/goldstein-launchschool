2460d24d87ca1b443707406df7b07d54d03626e3
jusgolds <jusgolds@gmail.com>
2018-08-18 16:45:40 -0500

    Update sudo code

--- small_problems/easy_3/odd_lists.rb
+++ small_problems/easy_3/odd_lists.rb
@@ -4,4 +4,18 @@
 # => that are in the 1st, 3rd, 5th, and so on elements of the argument Array.
 
 # def a method "oddities" with an argument arr
-# for index if odd, add to new arrary
+# create an odd elements arr in method
+# create index variable
+# while array.size > index
+# if index.even? add to odd_elements
+
+def oddities(arr)
+  arr.each_with_index { | num, index | if index.odd? < x }
+end
+
+#p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
+#p oddities(['abc', 'def']) == ['abc']
+#p oddities([123]) == [123]
+#p oddities([]) == []
+
+#p oddities([1,2,3])
