c845a67480559c9327bb439e1509ef25f3ee3ecd
jusgolds <jusgolds@gmail.com>
2018-08-18 17:26:19 -0500

    Sudo code

--- small_problems/easy_4/short_long_short.rb
+++ small_problems/easy_4/short_long_short.rb
@@ -3,3 +3,9 @@
 # => and then returns the result of concatenating
 # => the shorter string, the longer string, and the shorter string once again
 # => You may assume that the strings are of different lengths.
+
+# def method short_long_short(str, str2)
+# if string 1 is long than string 2
+# => concat 2-1-2
+# if string 2 is longer than 1
+# => concat 1-2-1
