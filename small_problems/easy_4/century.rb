830f59fdf2b13056b09421d9fa1617bb7591cf67
jusgolds <jusgolds@gmail.com>
2018-08-18 18:01:25 -0500

    Add century.rb iteration 1, year # only

--- small_problems/easy_4/century.rb
+++ small_problems/easy_4/century.rb
@@ -2,3 +2,28 @@
 # => The return value should be a string that begins with
 # => the century number
 # => and ends with st, nd, rd, or th as appropriate for that number.
+
+# => iteration 1: return the century # only
+# def method century(year)
+# if year % 100 != 0
+# century = year / 100 + 1
+# else
+# century = year / 100
+
+def century(year)
+  if year % 100 != 0
+    century = year / 100 + 1
+  else
+    century = year / 100
+  end
+end
+
+p century(2000)
+p century(2001)
+p century(1965)
+p century(256)
+p century(5)
+p century(10103)
+p century(1052)
+p century(1127)
+p century(11201)
