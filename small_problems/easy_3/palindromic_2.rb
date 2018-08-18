d4863cfd394987a5a549947c9214e31a396142ba
jusgolds <jusgolds@gmail.com>
2018-08-18 16:56:58 -0500

    Sudo code

--- small_problems/easy_3/palindromic_2.rb
+++ small_problems/easy_3/palindromic_2.rb
@@ -2,3 +2,8 @@
 # => if the string passed as an argument is a palindrome, false otherwise
 # => This time, however, your method should be case-insensitive
 # => and it should ignore all non-alphanumeric characters.
+
+# def method real_palindrome?(str)
+# new_str = remove str spaces and punctuation
+# new_str downcase
+# str == new_str
