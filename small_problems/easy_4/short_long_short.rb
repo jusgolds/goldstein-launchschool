c53baa739287895d7051a8a5653e9d65fc24e629
jusgolds <jusgolds@gmail.com>
2018-08-18 17:24:03 -0500

    Add easy_4/ and short_long_short

---
+++ small_problems/easy_4/short_long_short.rb
@@ -0,0 +1,5 @@
+# => Write a method that takes two strings as arguments
+# => determines the longest of the two strings
+# => and then returns the result of concatenating
+# => the shorter string, the longer string, and the shorter string once again
+# => You may assume that the strings are of different lengths.
