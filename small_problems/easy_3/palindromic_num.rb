1e0c036500c934265cfce0fcd2e52369e0fc6a9a
jusgolds <jusgolds@gmail.com>
2018-08-18 17:13:17 -0500

    Complete palindromic_num

--- small_problems/easy_3/palindromic_num.rb
+++ small_problems/easy_3/palindromic_num.rb
@@ -1,3 +1,20 @@
 # => Write a method that returns true if its integer argument
 # => is palindromic, false otherwise
 # => A palindromic number reads the same forwards and backwards.
+
+# def method palindromic_number?(int)
+# new_int = int.to_s
+# new_int = new_int.reverse
+# int == new_int
+
+def palindromic_number?(int)
+  new_int = int.to_s
+  new_int = new_int.reverse
+  new_int = new_int.to_i
+  int == new_int
+end
+
+p palindromic_number?(34543) == true
+p palindromic_number?(123210) == false
+p palindromic_number?(22) == true
+p palindromic_number?(5) == true
