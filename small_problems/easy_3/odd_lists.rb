0c4e9b7650dca70110b39177439204eb608329a1
jusgolds <jusgolds@gmail.com>
2018-08-18 16:30:45 -0500

    Add odd_lists

---
+++ small_problems/easy_3/odd_lists.rb
@@ -0,0 +1,4 @@
+# => Write a method that returns an Array that contains
+# => every other element of an Array that is passed in as an argument
+# => the values in the returned list should be those values
+# => that are in the 1st, 3rd, 5th, and so on elements of the argument Array.
