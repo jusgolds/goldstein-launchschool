352294c30310c194148f7ec2c5988761d3568803
jusgolds <jusgolds@gmail.com>
2018-08-18 16:53:48 -0500

    Complete palindromic_1

--- small_problems/easy_3/palindromic_1.rb
+++ small_problems/easy_3/palindromic_1.rb
@@ -6,3 +6,12 @@
 # def method palindrome?(str)
 # str == str.reverse
 # end
+
+def palindrome?(str)
+  str == str.reverse
+end
+
+p palindrome?('madam') == true
+p palindrome?('Madam') == false          # (case matters)
+p palindrome?("madam i'm adam") == false # (all characters matter)
+p palindrome?('356653') == true
