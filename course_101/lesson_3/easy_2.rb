7a9ac5c56187f7a441b05e7aa620d9ce66622ea0
jusgolds <jusgolds@gmail.com>
2018-08-18 16:29:13 -0500

    Add easy_2

--- course_101/lesson_3/easy_2.rb
+++ course_101/lesson_3/easy_2.rb
@@ -1,10 +1,38 @@
 # => 1
+# see if "Spot" is present
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+
+p ages.key?("Spot")
 
 # => 2
+# convert string in the following ways:
+# "The munsters are creepy in a good way."
+# "tHE mUNSTERS ARE CREEPY IN A GOOD WAY."
+# "the munsters are creepy in a good way."
+# "THE MUNSTERS ARE CREEPY IN A GOOD WAY."
+
+munsters_description = "The Munsters are creepy in a good way."
+puts munsters_description.capitalize
+puts munsters_description.swapcase
+puts munsters_description.downcase
+puts munsters_description.upcase
 
 # => 3
+# add ages for Marilyn and Spot to the ages hash
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10 }
+additional_ages = { "Marilyn" => 22, "Spot" => 237 }
+
+ages.merge!(additional_ages)
+
+p ages
 
 # => 4
+# See if the name "Dino" is in the string
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+p advice.match("Dino")
 
 # => 5
 
