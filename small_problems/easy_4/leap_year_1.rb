319102e80ed5642e85029cf3157a4f185ef84d38
jusgolds <jusgolds@gmail.com>
2018-08-19 15:28:57 -0500

    Add leap_year_1

---
+++ small_problems/easy_4/leap_year_1.rb
@@ -0,0 +1,5 @@
+# => In the modern era under the Gregorian Calendar
+# => leap years occur in every year that is evenly divisible by 4
+# => unless the year is also divisible by 100
+# => If the year is evenly divisible by 100
+# => then it is not a leap year unless the year is evenly divisible by 400.
