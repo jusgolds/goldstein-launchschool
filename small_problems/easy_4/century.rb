e2b81a8ebb1955bee531947ec8dd00fd2bf52eb1
jusgolds <jusgolds@gmail.com>
2018-08-19 15:26:16 -0500

    Complete century.rb

--- small_problems/easy_4/century.rb
+++ small_problems/easy_4/century.rb
@@ -7,16 +7,33 @@
 # if year % 100 == 0, century is year / 100
 # if year % 100 == 1 - 9, centiry is year /100 +1
 
-def century_year(year)
+# Iteration 2
+# create case statement to figure out suffix
+# based on number
+# 1st, 2nd, 3rd, 11th, 12th, 13th
+
+def century(year)
   if year % 100 == 0
     century = year / 100
-    puts century
   else
     century = (year / 100) + 1
-    puts century
+  end
+  century.to_s + century_suffix(century)
+end
+
+def century_suffix(century)
+  return 'th' if [11, 12, 13].include?(century % 100)
+  last_digit = century % 10
+
+  case last_digit
+  when 1 then 'st'
+  when 2 then 'nd'
+  when 3 then 'rd'
+  else 'th'
   end
 end
 
-century_year(1999)
-century_year(2000)
-century_year(2001)
+p century(1100)
+p century(1999)
+p century(2000)
+p century(2001)
