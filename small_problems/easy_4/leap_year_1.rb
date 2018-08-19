adfde81c2c449b3d21adbd783eb8dd32182fcd41
jusgolds <jusgolds@gmail.com>
2018-08-19 15:31:43 -0500

    Sudo code

--- small_problems/easy_4/leap_year_1.rb
+++ small_problems/easy_4/leap_year_1.rb
@@ -3,3 +3,11 @@
 # => unless the year is also divisible by 100
 # => If the year is evenly divisible by 100
 # => then it is not a leap year unless the year is evenly divisible by 400.
+
+# def leap_year?(year)
+# if year % 100 == 0 & year % 400 == 0
+# true
+# elsif year % 100 == 0 & year % 400 != 0
+# false
+# else
+# true
