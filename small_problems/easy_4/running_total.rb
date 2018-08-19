581f5021c5108b0b41ba6a76a58b602f524a831e
jusgolds <jusgolds@gmail.com>
2018-08-19 16:13:08 -0500

    Complete running_total

--- small_problems/easy_4/running_total.rb
+++ small_problems/easy_4/running_total.rb
@@ -8,3 +8,13 @@
 # loop through values in array
 # for each index add value to sum and append to totals
 # p totals
+
+def running_total(arr)
+  sum = 0
+  arr.map { |num| sum += num }
+end
+
+p running_total([2, 5, 13]) == [2, 7, 20]
+p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+p running_total([3]) == [3]
+p running_total([]) == []
