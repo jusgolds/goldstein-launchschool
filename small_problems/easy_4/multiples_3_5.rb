fae1b6822f45f9ed952fa5c5cccf990861206193
jusgolds <jusgolds@gmail.com>
2018-08-19 15:51:33 -0500

    Sudo code

--- small_problems/easy_4/multiples_3_5.rb
+++ small_problems/easy_4/multiples_3_5.rb
@@ -1,3 +1,11 @@
 # => Write a method that searches for all multiples of 3 or 5
 # => that lie between 1 and some other number
 # => and then computes the sum of those multiples.
+
+# def multisum(num)
+# counter = 0
+# arr = []
+# while counter <= num
+# if counter % 3 = 0 add to arr
+# if counter % 5 = 0 add to arr
+# total up arr
