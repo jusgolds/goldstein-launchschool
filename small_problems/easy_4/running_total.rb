1aa08d833b3975a64d5d618474e134816de5e131
jusgolds <jusgolds@gmail.com>
2018-08-19 16:03:36 -0500

    Sudo code

--- small_problems/easy_4/running_total.rb
+++ small_problems/easy_4/running_total.rb
@@ -1,3 +1,10 @@
 # => Write a method that takes an Array of numbers
 # => and returns an Array with the same number of elements
 # => and each element has the running total from the original Array.
+
+# def running_total(arr)
+# create a new array (totals)
+# sum = 0
+# loop through values in array
+# for each index add value to sum and append to totals
+# p totals
