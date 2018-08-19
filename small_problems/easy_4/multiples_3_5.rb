797fa2ac80582a005532514ebece183397c05412
jusgolds <jusgolds@gmail.com>
2018-08-19 15:59:44 -0500

    Complete multiples 3 and 5

--- small_problems/easy_4/multiples_3_5.rb
+++ small_problems/easy_4/multiples_3_5.rb
@@ -9,3 +9,20 @@
 # if counter % 3 = 0 add to arr
 # if counter % 5 = 0 add to arr
 # total up arr
+
+def multisum(num)
+  counter = 1
+  arr = []
+  while counter <= num
+    if counter % 3 == 0 || counter % 5 == 0
+      arr << counter
+    end
+    counter += 1
+  end
+  arr.sum
+end
+
+p multisum(3) == 3
+p multisum(5) == 8
+p multisum(10) == 33
+p multisum(1000) == 234168
