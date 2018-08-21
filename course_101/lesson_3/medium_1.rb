af051efb7f67eb004ecb7fb281cdcdf1404e45e3
jusgolds <jusgolds@gmail.com>
2018-08-21 15:52:26 -0500

    Add medium_1

---
+++ course_101/lesson_3/medium_1.rb
@@ -0,0 +1,5 @@
+# => 1
+# write a one-line program that creates an output 10 times
+# with the subsequent line indented 1 space to the right
+
+10.times { |number| puts (" " * number) + "The Flintstones rock" }
