dd5001ce08f0a89a3e7c39909d53a15770f29452
jusgolds <jusgolds@gmail.com>
2018-08-21 16:04:23 -0500

    Update medium_1.rb

--- course_101/lesson_3/medium_1.rb
+++ course_101/lesson_3/medium_1.rb
@@ -3,3 +3,40 @@
 # with the subsequent line indented 1 space to the right
 
 10.times { |number| puts (" " * number) + "The Flintstones rock" }
+
+# => 3
+
+#def factors(number)
+#  dividend = number
+#  divisors = []
+#  begin
+#    divisors << number / dividend if number % dividend == 0
+#    dividend -= 1
+#  end until dividend == 0
+#  divisors
+#end
+
+def factors(number)
+  dividend = number
+  divisors = []
+  while dividend > 0 do
+    divisors << number / dividend if number % dividend == 0
+    dividend -= 1
+  end
+  divisors
+end
+
+# => 5
+
+def fib(first_num, second_num)
+  limit = 15
+  while second_num < limit
+    sum = first_num + second_num
+    first_num = second_num
+    second_num = sum
+  end
+  sum
+end
+
+result = fib(0, 1)
+puts "result is #{result}"
