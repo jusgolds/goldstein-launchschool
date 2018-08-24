14ca7f2d8470baa2aa797c95ce4bc5b6638e8029
jusgolds <jusgolds@gmail.com>
2018-08-23 21:18:03 -0500

    Add after_midnight

---
+++ small_problems/easy_5/after_midnight.rb
@@ -0,0 +1,6 @@
+# => The time of day can be represented as
+# => the number of minutes before or after midnight
+# => If the number of minutes is positive, the time is after midnight
+# => If the number of minutes is negative, the time is before midnight.
+# => Write a method that takes a time using this minute-based format
+# => and returns the time of day in 24 hour format (hh:mm).
