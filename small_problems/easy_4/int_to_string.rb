31deee1cf2328db0d94cc0576f94fbfcbcd77299
jusgolds <jusgolds@gmail.com>
2018-08-23 20:05:20 -0500

    Sudo code

--- small_problems/easy_4/int_to_string.rb
+++ small_problems/easy_4/int_to_string.rb
@@ -1,2 +1,8 @@
 # => Write a method that takes a positive integer or zero
 # => and converts it to a string representation.
+
+# create a hash that has all of the integers to chars
+# create method
+# convert int to .digits.reverse
+# string = ''
+# num.each concat to string
