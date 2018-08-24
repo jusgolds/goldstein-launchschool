a1dba78674e6fb04b057e48f9a0f039db7405311
jusgolds <jusgolds@gmail.com>
2018-08-23 20:43:37 -0500

    Complete ascii_value

--- small_problems/easy_5/ascii_value.rb
+++ small_problems/easy_5/ascii_value.rb
@@ -1,2 +1,13 @@
 # => Write a method that determines and returns
 # => the ASCII string value of a string that is passed in as an argument
+
+def ascii_value(str)
+  value = 0
+  str.each_char { |c| value += c.ord }
+  p value
+end
+
+ascii_value('Four score')
+ascii_value('Launch School')
+ascii_value('a')
+ascii_value('')
