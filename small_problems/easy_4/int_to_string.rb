ce96accb9b9843d3ad12a877cf0f9ae563d489ff
jusgolds <jusgolds@gmail.com>
2018-08-23 20:14:05 -0500

    Complete int_to_string

--- small_problems/easy_4/int_to_string.rb
+++ small_problems/easy_4/int_to_string.rb
@@ -6,3 +6,20 @@
 # convert int to .digits.reverse
 # string = ''
 # num.each concat to string
+
+DIGITS = { 1 => '1', 2 => '2', 3 => '3', 4 => '4', 5 => '5',
+           6 => '6', 7 => '7', 8 => '8', 9 => '9', 0 => '0' }
+
+def integer_to_string(int)
+  int = int.digits.reverse
+
+  num = int.map { |i| DIGITS[i] }
+
+  string = ''
+  num.each { |n| string << n }
+  p string
+end
+
+integer_to_string(4321)
+integer_to_string(0)
+integer_to_string(5000)
