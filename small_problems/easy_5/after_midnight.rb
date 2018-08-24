a17e85584785c3360db7827402e9aed407db84f0
jusgolds <jusgolds@gmail.com>
2018-08-23 21:23:34 -0500

    Sudo code

--- small_problems/easy_5/after_midnight.rb
+++ small_problems/easy_5/after_midnight.rb
@@ -4,3 +4,7 @@
 # => If the number of minutes is negative, the time is before midnight.
 # => Write a method that takes a time using this minute-based format
 # => and returns the time of day in 24 hour format (hh:mm).
+
+# if number < 60, just the number of minutes to add
+# otherwise, total / 60 for hours & total % 60 for minutes
+# add these numbers to default time (00:00)
